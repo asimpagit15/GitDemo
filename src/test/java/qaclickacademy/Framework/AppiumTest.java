547564199c261c7b4db195c9c5e5048b9a23fd13
asim <asimpani8945@gmail.com>
2022-09-17 05:25:15 -0700

    first commit

---
+++ src/test/java/qaclickacademy/Framework/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Framework;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+@Test
+public void NativeAppAndroid()
+{
+	System.out.println("postJira");
+
+	}
+@Test
+public void IOSApps()
+{
+	System.out.println("IOSApps");
+}
+
+}
---
+++ src/test/java/qaclickacademy/Framework/RestTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Framework;
+
+import org.testng.annotations.Test;
+
+public class RestTest {
+
+	
+	@Test
+	public void postJira()
+	{
+		
+		System.out.println("postJira");
+	}
+	public void deletetwitter()
+	{
+		System.out.println("deletetwitter");
+	}
+
+}
